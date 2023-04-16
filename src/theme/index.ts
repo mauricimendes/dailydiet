0851cb5ad3c318dc2a928ef5c16057886a1b0095
mauricimendes <mauricimendes.14@gmail.com>
2023-04-16 03:06:03 -0300

    init

---
+++ src/theme/index.ts
@@ -0,0 +1,32 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+    GRAY_100: '#1B1D1E',
+    GRAY_200: '#333638',
+    GRAY_300: '#5C6265',
+    GRAY_400: '#B9BBBC',
+    GRAY_500: '#DDDEDF',
+    GRAY_600: '#DDDEDF',
+    GRAY_700: '#FAFAFA',
+
+    RED_DARK: '#BF3B44',
+    RED_MID: '#F3BABD',
+    RED_LIGHT: '#F4E6E7',
+
+    GREEN_DARK: '#639339',
+    GREEN_MID: '#CBE4B4',
+    GREEN_LIGHT: '#E5F0DB'
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Nunito_400Regular',
+    BOLD: 'Nunito_700Bold'
+  },
+  FONT_SIZE: {
+    EXTRA_SMALL: 12,
+    SMALL: 14,
+    MEDIUM_SMALL: 16,
+    MEDIUM: 18,
+    LARGE: 24,
+    EXTRA_LARGE: 32
+  }
+}
